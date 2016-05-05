0b767c815ec4926025052101d78508e713c36a18
Yair <rodriguezortega.yair@gmail.com>
2016-05-05 01:44:04 -0500

    JS: Add script to control translate property in “Blog Posts” section.

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -4,9 +4,12 @@ window.onload = function() {
       foreBird = document.querySelector('.fore-bird'),
       clothesPics = document.querySelector('.clothes-pics'),
       figures = Array.prototype.slice.call(document.querySelectorAll(".clothes-pics figure")),
-      offsetY, opacity,
+      offsetY, opacity, offsetPost,
       periscopeWindow = document.querySelector('.periscope'),
-      promoBlock = document.querySelector('.promo');
+      promoBlock = document.querySelector('.promo'),
+      blogContainer = document.querySelector('.blog-posts'),
+      firstPost = document.querySelector('.post-1'),
+      thirdPost = document.querySelector('.post-3');
 
 
   window.addEventListener('scroll', function() {
@@ -30,6 +33,12 @@ window.onload = function() {
       periscopeWindow.style.backgroundPosition = "center " + (offsetY - periscopeWindow.offsetTop) + "px";
       opacity = (offsetY - periscopeWindow.offsetTop + window.innerHeight / 2) / (offsetY / 5);
       promoBlock.style.opacity = opacity;
+    }
+
+    if(offsetY > blogContainer.offsetTop - window.innerHeight) {
+      offsetPost = Math.min(0, offsetY - blogContainer.offsetTop + window.innerHeight / 3);
+      firstPost.style.webkitTransform = "translate(" + offsetPost + "px, " + Math.abs(offsetPost * .2) + "px)";
+      thirdPost.style.webkitTransform = "translate(" + Math.abs(offsetPost) + "px, " + Math.abs(offsetPost * .2) + "px)";
     }
   });
 };
